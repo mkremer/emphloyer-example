b59ce1415e151ccee3ce26af521ac8bdf0a9f1c2
Mark Cornelissen <mark@5ec.nl>
2019-05-10 12:48:36 +0200

    Reformatted MakeThingJob

--- src/Emphloyer/Example/MakeThingJob.php
+++ src/Emphloyer/Example/MakeThingJob.php
@@ -1,19 +1,29 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Emphloyer\Example;
 
-class MakeThingJob extends \Emphloyer\AbstractJob {
-  public function __construct($name = "") {
-    $this->attributes['name'] = $name;
-  }
+use Emphloyer\AbstractJob;
+use Logger;
+use PDO;
+use function getenv;
 
-  public function perform() {
-    \Logger::getLogger(get_called_class())->info("Executing job, about to insert {$this->attributes['name']}");
-    $pdo = new \PDO("mysql:dbname=emphloyer_example;host=localhost", getenv("DB_USER"), getenv("DB_PWD"));
-
-    $stmt = $pdo->prepare('INSERT INTO things (name, created_at) VALUES (?, NOW())');
-    for ($i = 1; $i < 11; $i++) {
-      $stmt->execute(array($this->attributes['name'] . ' ' . $i));
+class MakeThingJob extends AbstractJob
+{
+    public function __construct($name = '')
+    {
+        $this->attributes['name'] = $name;
     }
-  }
+
+    public function perform() : void
+    {
+        Logger::getLogger(static::class)->info("Executing job, about to insert {$this->attributes['name']}");
+        $pdo = new PDO('mysql:dbname=emphloyer_example;host=localhost', getenv('DB_USER'), getenv('DB_PWD'));
+
+        $stmt = $pdo->prepare('INSERT INTO things (name, created_at) VALUES (?, NOW())');
+        for ($i = 1; $i < 11; $i++) {
+            $stmt->execute([$this->attributes['name'] . ' ' . $i]);
+        }
+    }
 }
